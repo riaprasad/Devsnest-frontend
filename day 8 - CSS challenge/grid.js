d44cba4b6294c2321e598e7467772ad071aee0e5
riaprasad <riaprasad46@gmail.com>
2021-06-29 12:20:16 +0530

    Day 12 THA

--- day 8 - CSS challenge/grid.js
+++
@@ -1,9 +0,0 @@
-const box = document.querySelector('.container');
-box.addEventListener('click', ()=> {
-  /*if(box.classList.contains('itemsclicked')){
-    box.classList.remove('itemsclicked')
-  }
-  else {*/
-    box.classList.add('itemsclicked')
-  /*}*/
-   })
